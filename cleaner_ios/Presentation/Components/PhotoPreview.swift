0d2b9a044ca43f75586d8f286875a0544aa90bff
Dmitry Yankovsky <dmitry.yankovsky@carrier.com>
2025-11-06 16:45:46 +0100

    feat: resize

--- cleaner_ios/Presentation/Components/PhotoPreview.swift
+++ cleaner_ios/Presentation/Components/PhotoPreview.swift
@@ -4,7 +4,9 @@ import SwiftUI
 struct PhotoPreview: View {
     @Environment(\.photoPreviewNamespace) var photoPreviewNamespace
     @ObservedObject var viewModel: PhotoViewModel
-    
+    @State private var image: UIImage?
+    @State private var isLoading = false
+
     private let targetSize = CGSize(width: 300, height: 400)
 
     var body: some View {
@@ -19,14 +21,25 @@ struct PhotoPreview: View {
                             closePreview()
                         }
 
-                    Rectangle()
-                        .fill(Color.gray.opacity(0.3))
-                        .cornerRadius(16)
-                        .matchedGeometryEffect(id: previewPhoto.id, in: namespace)
-                        .frame(width: targetSize.width, height: targetSize.height)
-                        .onTapGesture {
-                            closePreview()
-                        }
+                    if let image = image {
+
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(16)
+                            .opacity(0.5)
+                            .matchedGeometryEffect(id: previewPhoto.id, in: namespace)
+                            .onTapGesture {
+                                closePreview()
+                            }
+                    }
+                }
+                .onAppear {
+                    loadImage()
+                }
+                .onChange(of: viewModel.previewPhoto?.id) { _ in
+                    print("🔄 previewPhoto changed to: \(viewModel.previewPhoto?.id)")
+                    loadImage()
                 }
                 .ignoresSafeArea()
                 .zIndex(1000)
@@ -38,6 +51,35 @@ struct PhotoPreview: View {
         withAnimation(.spring(response: 3, dampingFraction: 1)) {
             viewModel.showPreviewModel = false
             viewModel.previewPhoto = nil
+        }
+    }
+
+    private func loadImage() {
+        print("🔄 loadImage")
+        guard let asset = viewModel.previewPhoto?.asset else {
+            return
+        }
+        print("🔄 asset: \(asset)")
+        isLoading = true
+        
+        let options = PHImageRequestOptions()
+        options.isSynchronous = false
+        options.deliveryMode = .opportunistic 
+        options.resizeMode = .fast
+        options.isNetworkAccessAllowed = false // Не загружать из iCloud при скролле
+
+        
+        PHImageManager.default().requestImage(
+            for: asset,
+            targetSize: targetSize,
+            contentMode: .aspectFill,
+            options: options
+        ) { result, _ in
+            DispatchQueue.main.async {
+                print("🔄 result: \(result)")
+                self.image = result
+                self.isLoading = false
+            }
         }
     }
 }
